21f2b394026e3dae269a897834585352e0c34758
sgcx198 <liwenyi@songguo7.com>
2020-05-08 09:40:29 +0800

    update

---
+++ code_week2_54_58/minimum_absolute_difference_in_bst.py
@@ -0,0 +1,49 @@
+'''
+给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+
+ 
+
+示例：
+
+输入：
+
+   1
+    \
+     3
+    /
+   2
+
+输出：
+1
+
+解释：
+最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        st = []
+        p = root
+        pre = -float('inf')
+        min_val = float('inf')
+        while p is not None or st:
+            while p is not None:
+                st.append(p)
+                p = p.left
+            p = st.pop()
+            cur = p.val
+            if cur - pre < min_val:
+                min_val = cur - pre
+            pre = cur
+            p = p.right
+        return min_val
