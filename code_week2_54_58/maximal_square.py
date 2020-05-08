e2b55e3a426aff490a53c46efdace5f9540feba8
sgcx198 <liwenyi@songguo7.com>
2020-05-08 09:13:43 +0800

    update

---
+++ code_week2_54_58/maximal_square.py
@@ -0,0 +1,34 @@
+'''
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+
+示例:
+
+输入:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+输出: 4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximal-square
+'''
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        maxSide = 0
+        rows,columns = len(matrix),len(matrix[0])
+        dp = [[0] * columns for _ in range(rows)]
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1]) + 1
+                    maxSide = max(maxSide,dp[i][j])
+        maxSquare = maxSide ** 2
+        return maxSquare
