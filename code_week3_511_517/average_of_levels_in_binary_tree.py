23d718a4d80dd3e623711239d1bf1f89715d2447
sgcx198 <liwenyi@songguo7.com>
2020-05-13 09:19:50 +0800

    update

---
+++ code_week3_511_517/average_of_levels_in_binary_tree.py
@@ -0,0 +1,43 @@
+'''
+给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
+
+示例 1:
+
+输入:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+输出: [3, 14.5, 11]
+解释:
+第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/average-of-levels-in-binary-tree
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        queue = collections.deque()
+        res = []
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            level = []
+            for _ in range(size):
+                cur = queue.popleft()
+                if not cur:
+                    continue
+                level.append(cur.val)
+                queue.append(cur.left)
+                queue.append(cur.right)
+            if level:
+                res.append(sum(level) / len(level))
+        return res
