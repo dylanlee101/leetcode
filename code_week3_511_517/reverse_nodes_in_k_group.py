0a2634437d59e7c228ed296684673c4fd55f8080
sgcx198 <liwenyi@songguo7.com>
2020-05-16 22:43:19 +0800

    update

---
+++ code_week3_511_517/reverse_nodes_in_k_group.py
@@ -0,0 +1,65 @@
+'''
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+ 
+
+示例：
+
+给你这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+ 
+
+说明：
+
+你的算法只能使用常数的额外空间。
+你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverse(self,head,tail):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail,head
+
+
+
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        head_prev = ListNode(0)
+        head_prev.next = head
+        pre = head_prev
+
+        while head:
+            tail = pre
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return head_prev.next
+            nex = tail.next
+            head,tail = self.reverse(head,tail)
+
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+        return head_prev.next
