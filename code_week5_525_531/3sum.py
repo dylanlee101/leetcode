f0608196269f0ab0225cf7a02c3c523c6c27c1a1
sgcx198 <liwenyi@songguo7.com>
2020-05-26 10:01:47 +0800

    update

---
+++ code_week5_525_531/3sum.py
@@ -0,0 +1,49 @@
+'''
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+ 
+
+示例：
+
+给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+
+满足要求的三元组集合为：
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/3sum
+'''
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        if not nums or n < 3:
+            return []
+        nums.sort()
+        for i in range(n):
+            if nums[i] > 0:
+                return res
+            if i >0 and nums[i] == nums[i-1]:
+                continue
+            L = i+1
+            R = n - 1
+            while L < R:
+                # print(i,L,R,n)
+                if (nums[i] + nums[L] + nums[R] == 0):
+                    res.append([nums[i] , nums[L] , nums[R]])
+                    while L < R and nums[L] == nums[L+1]:
+                        L = L + 1
+                    while L < R and nums[R] == nums[R - 1]:
+                        R = R -1
+                    L = L + 1
+                    R = R -1
+                elif (nums[i] + nums[L] + nums[R] < 0):
+                    L = L+ 1
+                else:
+                    R = R - 1
+        return res
