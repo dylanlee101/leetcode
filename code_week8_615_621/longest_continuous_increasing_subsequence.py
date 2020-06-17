cb32926406f3fc894deda8179c648ba75542f7fe
sgcx198 <liwenyi@songguo7.com>
2020-06-17 09:33:25 +0800

    update

---
+++ code_week8_615_621/longest_continuous_increasing_subsequence.py
@@ -0,0 +1,28 @@
+'''
+给定一个未经排序的整数数组，找到最长且连续的的递增序列，并返回该序列的长度。
+
+ 
+
+示例 1:
+
+输入: [1,3,5,4,7]
+输出: 3
+解释: 最长连续递增序列是 [1,3,5], 长度为3。
+尽管 [1,3,5,7] 也是升序的子序列, 但它不是连续的，因为5和7在原数组里被4隔开。
+示例 2:
+
+输入: [2,2,2,2,2]
+输出: 1
+解释: 最长连续递增序列是 [2], 长度为1。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-continuous-increasing-subsequence
+'''
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        ans = anchor = 0
+        for i in range(len(nums)):
+            if i and nums[i-1] >= nums[i] : anchor = i
+            ans = max(ans,i-anchor+1)
+        return ans
+
