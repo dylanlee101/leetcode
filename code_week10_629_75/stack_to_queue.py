023e6a01c325c0d470c2bfce5cfd8ac87c838bed
sgcx198 <liwenyi@songguo7.com>
2020-06-30 08:56:18 +0800

    update

---
+++ code_week10_629_75/stack_to_queue.py
@@ -0,0 +1,52 @@
+'''
+用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+
+ 
+
+示例 1：
+
+输入：
+["CQueue","appendTail","deleteHead","deleteHead"]
+[[],[3],[],[]]
+输出：[null,null,3,-1]
+示例 2：
+
+输入：
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+输出：[null,-1,null,null,5,2]
+提示：
+
+1 <= values <= 10000
+最多会对 appendTail、deleteHead 进行 10000 次调用
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+'''
+class CQueue:
+
+    def __init__(self):
+        self.stack = []
+        self.stack_ass = []
+
+
+    def appendTail(self, value: int) -> None:
+        self.stack.append(value)
+
+
+    def deleteHead(self) -> int:
+        if not self.stack_ass:
+            if not self.stack:
+                return -1
+            else:
+                while self.stack:
+                    self.stack_ass.append(self.stack.pop())
+        return self.stack_ass.pop()
+
+
+
+
+# Your CQueue object will be instantiated and called as such:
+# obj = CQueue()
+# obj.appendTail(value)
+# param_2 = obj.deleteHead()
