afbee74e4685b37d1c3d339b073c90fdac5bfcf9
sgcx198 <liwenyi@songguo7.com>
2020-06-30 09:14:03 +0800

    update

---
+++ code_week10_629_75/convert_sorted_list_to_binary_search_tree.py
@@ -0,0 +1,57 @@
+'''
+给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+
+本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+
+示例:
+
+给定的有序链表： [-10, -3, 0, 5, 9],
+
+一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+'''
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        size = 0
+        p = head
+        while p:
+            size += 1
+            p = p.next
+
+        def rebuildTree(l, r):
+            nonlocal head
+            if l >= r:
+                return None
+            mid = (l + r) // 2
+            left = rebuildTree(l, mid)
+            root = TreeNode(head.val)
+            root.left = left
+            head = head.next
+            root.right = rebuildTree(mid + 1, r)
+
+            return root
+
+        return rebuildTree(0, size)
