23a98d157f6691f948ba988869230571aa66d793
sgcx198 <liwenyi@songguo7.com>
2020-07-03 09:19:30 +0800

    update

---
+++ code_week10_629_75/pascals_triangle.py
@@ -0,0 +1,34 @@
+'''
+给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+
+
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 5
+输出:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/pascals-triangle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = []
+        for row_num in range(numRows):
+            row = [None for _ in range (row_num+1)]
+            row[0],row[-1] = 1,1
+
+            for j in range(1,len(row) - 1):
+                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+            triangle.append(row)
+        return triangle
