331f2c849bff37ab3b757651e527311d34a7ec66
sgcx198 <liwenyi@songguo7.com>
2020-07-07 09:15:33 +0800

    update

---
+++ code_week11_76_712/path_sum.py
@@ -0,0 +1,47 @@
+'''
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        que_node = collections.deque([root])
+        que_val = collections.deque([root.val])
+        while que_node:
+            now = que_node.popleft()
+            temp = que_val.popleft()
+            if not now.left and not now.right:
+                if temp == sum:
+                    return True
+                continue
+            if now.left:
+                que_node.append(now.left)
+                que_val.append(now.left.val + temp)
+            if now.right:
+                que_node.append(now.right)
+                que_val.append(now.right.val + temp)
+        return False
