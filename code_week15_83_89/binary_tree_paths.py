b5fb4e993cad8198b082811c39dc6b415cda214c
sgcx198 <liwenyi@songguo7.com>
2020-08-06 09:25:06 +0800

    update

---
+++ code_week15_83_89/binary_tree_paths.py
@@ -0,0 +1,44 @@
+'''
+给定一个二叉树，返回所有从根节点到叶子节点的路径。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+输入:
+
+   1
+ /   \
+2     3
+ \
+  5
+
+输出: ["1->2->5", "1->3"]
+
+解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-paths
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+
+        def construt_paths(root,path):
+            if root:
+                path += str(root.val)
+                if not root.left and not root.right:
+                    paths.append(path)
+                else:
+                    path += '->'
+                    construt_paths(root.left,path)
+                    construt_paths(root.right,path)
+        paths = []
+        construt_paths(root,'')
+        return paths
