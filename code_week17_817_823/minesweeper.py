967baaedce6a0bce7301eb33dfd1c68f834b4f94
sgcx198 <liwenyi@songguo7.com>
2020-08-20 20:34:44 +0800

    update

---
+++ code_week17_817_823/minesweeper.py
@@ -0,0 +1,87 @@
+'''
+让我们一起来玩扫雷游戏！
+
+给定一个代表游戏板的二维字符矩阵。 'M' 代表一个未挖出的地雷，'E' 代表一个未挖出的空方块，'B' 代表没有相邻（上，下，左，右，和所有4个对角线）地雷的已挖出的空白方块，数字（'1' 到 '8'）表示有多少地雷与这块已挖出的方块相邻，'X' 则表示一个已挖出的地雷。
+
+现在给出在所有未挖出的方块中（'M'或者'E'）的下一个点击位置（行和列索引），根据以下规则，返回相应位置被点击后对应的面板：
+
+如果一个地雷（'M'）被挖出，游戏就结束了- 把它改为 'X'。
+如果一个没有相邻地雷的空方块（'E'）被挖出，修改它为（'B'），并且所有和其相邻的未挖出方块都应该被递归地揭露。
+如果一个至少与一个地雷相邻的空方块（'E'）被挖出，修改它为数字（'1'到'8'），表示相邻地雷的数量。
+如果在此次点击中，若无更多方块可被揭露，则返回面板。
+ 
+
+示例 1：
+
+输入:
+
+[['E', 'E', 'E', 'E', 'E'],
+ ['E', 'E', 'M', 'E', 'E'],
+ ['E', 'E', 'E', 'E', 'E'],
+ ['E', 'E', 'E', 'E', 'E']]
+
+Click : [3,0]
+
+输出:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'M', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+解释:
+
+示例 2：
+
+输入:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'M', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+Click : [1,2]
+
+输出:
+
+[['B', '1', 'E', '1', 'B'],
+ ['B', '1', 'X', '1', 'B'],
+ ['B', '1', '1', '1', 'B'],
+ ['B', 'B', 'B', 'B', 'B']]
+
+解释:
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minesweeper
+'''
+
+
+class Solution:
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            return board
+        self.m = len(board)
+        self.n = len(board[0])
+        bookAround = ((1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1))
+
+        def check(i, j):
+            num = 0
+            for x, y in bookAround:
+                if 0 <= x + i < self.m and 0 <= y + j < self.n and board[x + i][y + j] == 'M':
+                    num += 1
+            return num
+
+        def dfs(i, j):
+            num = check(i, j)
+            if num:
+                board[i][j] = str(num)
+            else:
+                board[i][j] = 'B'
+                for x, y in bookAround:
+                    if 0 <= x + i < self.m and 0 <= y + j < self.n and board[x + i][y + j] == 'E':
+                        dfs(x + i, y + j)
+
+        dfs(click[0], click[1])
+        return board
