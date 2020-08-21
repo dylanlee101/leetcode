8e5cf142a8f699eff5b8aeb06025953c2b8c55b1
sgcx198 <liwenyi@songguo7.com>
2020-08-21 09:09:04 +0800

    update

---
+++ code_week17_817_823/minmum_depth_of_binary_tree.py
@@ -0,0 +1,48 @@
+'''
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例:
+
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最小深度  2.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import queue
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        q = queue.Queue()
+        depth = 1
+        q.put(root)
+        while not q.empty():
+            sz = q.qsize()
+            for i in range(sz):
+                cur = q.get()
+                if cur.left == None and cur.right == None:
+                    return depth
+                if cur.left != None:
+                    q.put(cur.left)
+                if cur.right != None:
+                    q.put(cur.right)
+            depth += 1
+        return depth
