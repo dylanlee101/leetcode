3302164f1e1c6da1e6feb5a1837197b5f1233a0f
sgcx198 <liwenyi@songguo7.com>
2020-09-07 09:40:24 +0800

    update

---
+++ code_week20_97_913/merge_two_sorted_lists.py
@@ -0,0 +1,35 @@
+'''
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        prehead = ListNode(-1)
+        prev = prehead
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+        prev.next = l1 if l1 is not None else l2
+
+        return prehead.next
