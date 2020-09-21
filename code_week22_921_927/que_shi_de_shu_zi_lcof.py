16e2b9bc68b0f43c9ff80ad4a0c7e76adf575cba
sgcx198 <liwenyi@songguo7.com>
2020-09-21 09:38:05 +0800

    update

---
+++ code_week22_921_927/que_shi_de_shu_zi_lcof.py
@@ -0,0 +1,29 @@
+'''
+一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+
+ 
+
+示例 1:
+
+输入: [0,1,3]
+输出: 2
+示例 2:
+
+输入: [0,1,2,3,4,5,6,7,9]
+输出: 8
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof
+'''
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = left + (right - left ) // 2
+            if nums[mid] == mid :
+                left = mid+1
+            else:
+                right = mid -1
+        return left
