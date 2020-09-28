dd529d7436af78c6084dbe7af86c14869ab06ac2
sgcx198 <liwenyi@songguo7.com>
2020-09-28 09:46:56 +0800

    update

---
+++ code_week23_928_104/populating_next_right_pointers_in_each_node_ii.py
@@ -0,0 +1,64 @@
+'''
+给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+ 
+
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ 
+
+示例：
+
+
+
+输入：root = [1,2,3,4,5,null,7]
+输出：[1,#,2,3,#,4,5,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ 
+
+提示：
+
+树中的节点数小于 6000
+-100 <= node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+'''
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        stack = [(root,0)]
+        while True:
+            node,level = stack.pop(0)
+            if node.left :
+                stack.append((node.left,level+1))
+            if node.right :
+                stack.append((node.right,level+1))
+            if not stack :
+                break
+            if level == stack[0][1]:
+                node.next = stack[0][0]
+        return root
