b2036dd3a55d7cb53c973b5e5384466f88f5859e
sgcx198 <liwenyi@songguo7.com>
2020-09-29 09:22:02 +0800

    update

---
+++ code_week23_928_104/binary_tree_postorder_traversal.py
@@ -0,0 +1,36 @@
+'''
+给定一个二叉树，返回它的 后序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [3,2,1]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        stack,rst = [root],[]
+
+        while stack:
+            i = stack.pop()
+            if isinstance(i,TreeNode):
+                stack.extend([i.val,i.right,i.left])
+            elif isinstance(i,int):
+                rst.append(i)
+        return rst
