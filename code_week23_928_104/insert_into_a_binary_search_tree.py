8ef92520bb191e0ddcb93083fd564a7ba25b187f
sgcx198 <liwenyi@songguo7.com>
2020-09-30 09:52:00 +0800

    update

---
+++ code_week23_928_104/insert_into_a_binary_search_tree.py
@@ -0,0 +1,63 @@
+'''
+给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据保证，新值和原始二叉搜索树中的任意节点值都不同。
+
+注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回任意有效的结果。
+
+ 
+
+例如, 
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和 插入的值: 5
+你可以返回这个二叉搜索树:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+或者这个树也是有效的:
+
+         5
+       /   \
+      2     7
+     / \
+    1   3
+         \
+          4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/insert-into-a-binary-search-tree
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        pos = root
+        while pos:
+            if val < pos.val:
+                if not pos.left :
+                    pos.left = TreeNode(val)
+                    break
+                else:
+                    pos = pos.left
+            else:
+                if not pos.right:
+                    pos.right = TreeNode(val)
+                    break
+                else:
+                    pos = pos.right
+        return root
