a42be973e9af674ca3bfc08fe64a536399c560bb
sgcx198 <liwenyi@songguo7.com>
2020-10-09 10:01:04 +0800

    update

---
+++ code_week24_105_1011/cong_shang_dao_xia_da_yin.py
@@ -0,0 +1,46 @@
+'''
+从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+
+ 
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof
+
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root : return []
+        res,queue = [],collections.deque()
+        queue.append(root)
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left:queue.append(node.left)
+                if node.right:queue.append(node.right)
+            res.append(tmp)
+        return res
