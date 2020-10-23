2997311d4f201da91f78cf42c35c11279fb700f7
sgcx198 <liwenyi@songguo7.com>
2020-10-23 09:53:39 +0800

    update

---
+++ code_week26_1019_1025/cong_shang_dao_xia_da_yin_er_cha_shu_iii.py
@@ -0,0 +1,46 @@
+'''
+请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+
+ 
+
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res,deque = [],collections.deque([root])
+        while deque:
+            tmp = collections.deque()
+            for _ in range(len(deque)):
+                node = deque.popleft()
+                if len(res) % 2 : tmp.appendleft(node.val)
+                else:tmp.append(node.val)
+                if node.left:deque.append(node.left)
+                if node.right: deque.append(node.right)
+            res.append(list(tmp))
+        return res
