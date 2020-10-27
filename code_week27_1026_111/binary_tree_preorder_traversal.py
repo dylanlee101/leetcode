a8f7b0731cb7be1b64849ab59756a031d38f08ce
sgcx198 <liwenyi@songguo7.com>
2020-10-27 09:16:32 +0800

    update

---
+++ code_week27_1026_111/binary_tree_preorder_traversal.py
@@ -0,0 +1,34 @@
+'''
+给定一个二叉树，返回它的 前序 遍历。
+
+ 示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,2,3]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-preorder-traversal
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        stack,rst = [root],[]
+        while stack:
+            i = stack.pop()
+            if isinstance(i,TreeNode):
+                stack.extend([i.right,i.left,i.val])
+            elif isinstance(i,int):
+                rst.append(i)
+        return rst
