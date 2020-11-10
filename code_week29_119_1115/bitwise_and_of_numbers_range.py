25e6d3e71208d83102a970c6ad101ea48c1b7062
sgcx198 <liwenyi@songguo7.com>
2020-11-10 09:57:10 +0800

    update

---
+++ code_week29_119_1115/bitwise_and_of_numbers_range.py
@@ -0,0 +1,26 @@
+'''
+给定范围 [m, n]，其中 0 <= m <= n <= 2147483647，返回此范围内所有数字的按位与（包含 m, n 两端点）。
+
+示例 1: 
+
+输入: [5,7]
+输出: 4
+示例 2:
+
+输入: [0,1]
+输出: 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/bitwise-and-of-numbers-range
+'''
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        shift = 0
+        # 找到公共前缀
+        while m < n:
+            m = m >> 1
+            n = n >> 1
+            shift += 1
+        return m << shift
+
+
