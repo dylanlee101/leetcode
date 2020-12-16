cd2ba4f2213e8f5e7a7ed81ae5e2e8f36ceae5ed
sgcx198 <liwenyi@songguo7.com>
2020-12-16 09:45:31 +0800

    update

---
+++ code_week34_1214_1220/word_pattern.py
@@ -0,0 +1,39 @@
+'''
+给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+
+这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+
+示例1:
+
+输入: pattern = "abba", str = "dog cat cat dog"
+输出: true
+示例 2:
+
+输入:pattern = "abba", str = "dog cat cat fish"
+输出: false
+示例 3:
+
+输入: pattern = "aaaa", str = "dog cat cat dog"
+输出: false
+示例 4:
+
+输入: pattern = "abba", str = "dog dog dog dog"
+输出: false
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/word-pattern
+'''
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        word2ch = dict()
+        ch2word = dict()
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        for ch,word in zip(pattern,words):
+            if (word in word2ch and word2ch[word] != ch) or (ch in ch2word and ch2word[ch] != word):
+                return False
+            word2ch[word] = ch
+            ch2word[ch] = word
+
+        return True
